f9c5de350fcec4fbf19e1d8933f4c18fd0598dec
Arjun Gupta <118122439+arjungupt771@users.noreply.github.com>
2025-05-28 09:12:52 +0530

    Add files via upload

---
+++ Video_Analyzer/questions.py
@@ -0,0 +1,7 @@
+qa_set = {
+    "Could you elaborate on your core competencies and key skills ?": "A linked list is a linear data structure where elements are stored in nodes and connected via pointers.",
+    "What activities or responsibilities do you find most fulfilling in your work?": "Polymorphism is the ability of different objects to respond uniquely to the same method call.",
+    "Could you provide an overview of your educational qualifications and professional experience ?":"",
+    "Can you share some of your significant achievements and how they were accomplished?":"",
+    "What are your aspirations for the future, both professionally and personally ?":""
+}
